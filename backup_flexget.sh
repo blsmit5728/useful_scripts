8a6ad555c34641e698ea6883f43b019b2f1baec2
Brandon Smith <blsmit5728@gmail.com>
2013-12-19 11:29:59 -0500

    Fixed location of tar.

--- backup_flexget.sh
+++ backup_flexget.sh
@@ -4,9 +4,9 @@ DATE=`date +%m_%d_%Y`
 
 if [ -d /home/bsmith/Dropbox ]
 then
-    tar -cvzf flexget_backup_${DATE}.tar.gz -C /home/bsmith/repos/flexget_files/ . >> /home/bsmith/Dropbox/.copy_flexget
+    tar -cvzf /home/bsmith/Dropbox/flexget/flexget_backup_${DATE}.tar.gz -C /home/bsmith/repos/flexget_files/ . >> /home/bsmith/Dropbox/.copy_flexget
 else
-    tar -cvzf flexget_backup_${DATE}.tar.gz -C /home/bsmith/repos/flexget_files/ .
+    tar -cvzf /home/bsmith/flexget_backup_${DATE}.tar.gz -C /home/bsmith/repos/flexget_files/ .
 fi
 
 
